82c57e090c1159cc41a337a2c6011c28904e6c31
Kavyas099 <kavyakummithareddy@gmail.com>
2026-02-03 23:47:08 +0530

    add

--- output.tf
+++ output.tf
@@ -7,13 +7,13 @@ output "vpc_id" {
 }
 
 output "public_subnet_id" {
-  value = aws_subnet.public[*].id
+  value = aws_subnet.public_subnet[*].id
 }
 
 output "private_subnet_id" {
-  value = aws_subnet.private[*].id
+  value = aws_subnet.private_subnet[*].id
 }
 
 output "database_subnet_id" {
-  value = aws_subnet.database[*].id
+  value = aws_subnet.database_subnet[*].id
 }
